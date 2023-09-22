834798361a612723e9677a16a66602b7eb3c07e7
Simon1511 <simon2002.schoenmackers@gmail.com>
2023-09-22 16:57:06 +0200

    a71: Enforcing bringup for S
    * Based on https://github.com/LineageOS/android_device_samsung_sm7125-common/commit/49c927e6d406093d790b115c0214d1942ab4c348.
    
    Change-Id: I787240afe50787666ab393ce88b55b882d48672b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,7 +160,7 @@ BOARD_BOOT_HEADER_VERSION := 2
 BOARD_DTB_OFFSET := 0x01F00000
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 4096
