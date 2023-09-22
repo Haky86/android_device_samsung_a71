ca454306763e8808076ffd3f176c5af5d4513ec1
Haky86 <hakamelassouad@gmail.com>
2023-09-22 16:12:41 +0200

    a71: power: Switch to the OSS QTI Power HAL
    * Needed for DT2W stuff.
    * Taken from vendor/qcom/opensource.
    
    Change-Id: I90d534d54598f959b26002b8ad9d2bf5abc8bb8e

--- device.mk
+++ device.mk
@@ -472,7 +472,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service-qti.sm6150 \
 
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/etc/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
