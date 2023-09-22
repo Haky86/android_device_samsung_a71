6a64c72922cc27c0e5e8f30e06ef9dab5085164a
Haky86 <hakamelassouad@gmail.com>
2023-09-22 16:55:54 +0200

    a71: Install everything into `/vendor` partition
    * Remove some ramdisk scripts, don't remove other scripts since they're used by dual sim variants.
    * Install every needed hidl into /vendor.
    * Drop /system hidls, this move fixes most "this hal is not registered" errors.
    * Drop OVERRIDE_RS_DRIVER, this flag is no longer used.
    
    Change-Id: I156608e362ebc9637b47866bcf260c3f94580d8a

--- device.mk
+++ device.mk
@@ -55,7 +55,6 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     android.hardware.audio@6.0-impl.sm6150 \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.common-util \
     android.hardware.audio.common-util.vendor \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
@@ -132,7 +131,6 @@ PRODUCT_COPY_FILES += \
 
 # Automotive/Gearhead
 PRODUCT_PACKAGES += \
-    android.hardware.automotive.vehicle@2.0 \
     android.hardware.automotive.vehicle@2.0.vendor \
     android.hardware.automotive.vehicle@2.0-manager-lib \
 
@@ -141,7 +139,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     aptxalsOverlay \
     audio.bluetooth.default \
-    com.qualcomm.qti.bluetooth_audio@1.0 \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libbluetooth_audio_session \
     libbluetooth_qti \
@@ -154,11 +151,9 @@ PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.common@2.0.vendor \
     android.frameworks.cameraservice.device@2.0.vendor \
     android.frameworks.cameraservice.service@2.1.vendor \
-    android.hardware.camera.device@3.7 \
     android.hardware.camera.device@3.7.vendor \
     android.hardware.camera.provider@2.5-impl \
     android.hardware.camera.provider@2.6-service.sm6150 \
-    android.hardware.camera.provider@2.7 \
     android.hardware.camera.provider@2.7.vendor \
     libcamera2ndk_vendor \
     libdng_sdk \
@@ -196,7 +191,6 @@ PRODUCT_PACKAGES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3 \
     android.hardware.drm@1.3.vendor \
 
 # Fastbootd
@@ -205,9 +199,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.face@1.0 \
     android.hardware.biometrics.face@1.0.vendor \
-    android.hardware.biometrics.fingerprint@2.1 \
     android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint@2.3-service-samsung.sm6150 \
 
@@ -229,10 +221,6 @@ PRODUCT_PACKAGES += \
 
 # GNSS/GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0 \
-    android.hardware.gnss@1.1 \
-    android.hardware.gnss@2.0 \
-    android.hardware.gnss@2.1 \
     android.hardware.gnss@2.1-service-qti \
     android.hardware.gnss@2.1.vendor \
 
@@ -263,35 +251,7 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libion \
     libvulkan \
-    vendor.display.color@1.0 \
-    vendor.display.color@1.1 \
-    vendor.display.color@1.2 \
-    vendor.display.color@1.3 \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.1 \
-    vendor.display.config@1.2 \
-    vendor.display.config@1.3 \
-    vendor.display.config@1.4 \
-    vendor.display.config@1.5 \
-    vendor.display.config@2.0 \
-    vendor.display.postproc@1.0 \
-    vendor.display.color@1.0.vendor \
-    vendor.display.color@1.1.vendor \
-    vendor.display.color@1.2.vendor \
-    vendor.display.color@1.3.vendor \
-    vendor.display.color@1.4.vendor \
     vendor.display.color@1.5.vendor \
-    vendor.display.config@1.0.vendor \
-    vendor.display.config@1.1.vendor \
-    vendor.display.config@1.2.vendor \
-    vendor.display.config@1.3.vendor \
-    vendor.display.config@1.4.vendor \
-    vendor.display.config@1.5.vendor \
-    vendor.display.config@1.6.vendor \
-    vendor.display.config@1.7.vendor \
-    vendor.display.config@1.8.vendor \
-    vendor.display.config@1.9.vendor \
-    vendor.display.config@1.10.vendor \
     vendor.display.config@1.11.vendor \
     vendor.display.config@2.0.vendor \
     vendor.display.postproc@1.0.vendor \
@@ -347,14 +307,10 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libavservices_minijail \
     libavservices_minijail.vendor \
     libavservices_minijail_vendor \
-    libcodec2_hidl@1.0 \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_hidl@1.1 \
     libcodec2_hidl@1.1.vendor \
-    libcodec2_vndk \
     libcodec2_vndk.vendor \
     libsfplugin_ccodec \
 
@@ -396,8 +352,6 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0:64 \
-    android.hardware.nfc@1.1:64 \
     android.hardware.nfc@1.2:64 \
     com.android.nfc_extras \
     libchrome \
@@ -501,7 +455,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti.sm6150 \
     android.hardware.power.stats@1.0-service.mock \
-    android.hardware.power@1.3 \
     android.hardware.power@1.3.vendor \
 
 PRODUCT_COPY_FILES += \
@@ -577,9 +530,6 @@ PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
-    init.qti.dcvs.sh \
-    init.qti.qcv.sh \
-    qca6234-service.sh \
 
 # Recovery
 AB_OTA_UPDATER := false
@@ -593,13 +543,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.5 \
     android.hardware.radio@1.5.vendor \
-    android.hardware.radio.config@1.2 \
     android.hardware.radio.config@1.2.vendor \
-    android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.0.vendor \
     librilutils \
     libxml2 \
     secril_config_svc \
@@ -622,7 +569,6 @@ PRODUCT_PACKAGES += \
     sensors.samsung \
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/rootdir/etc/permissions/com.samsung.permission.SSENSOR.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.samsung.permission.SSENSOR.xml \
     $(DEVICE_PATH)/rootdir/vendor/etc/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(DEVICE_PATH)/rootdir/vendor/etc/sensors/sns_reg_config:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sns_reg_config \
 
@@ -662,7 +608,6 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb.gadget@1.0-service-qti \
-    android.hardware.usb@1.0.vendor \
     android.hardware.usb@1.1.vendor \
 
 # Vendor Service Manager
