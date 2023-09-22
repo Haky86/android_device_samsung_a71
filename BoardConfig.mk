c6883f82ba9877d48d4b14993d2789b2d50808c4
Albert I <kras@raphielgang.org>
2023-09-22 15:02:52 +0200

    a71: Setup dexpreopt configuration
    * Due to LineageOS/android_build@28d0698e8, eng and userdebug builds are
    only having boot and system server dexpreopt-ed thus need to explicitly
    disable WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY on device
    makefile to enable full dexpreopt for userdebug builds.
    
    * Also, remove useless WITH_DEXPREOPT flag from device makefile since it
    defaults to true in the build system and WILL never be set.
    
    Change-Id: I459dcd5fa05d8406c6100598d8b9001662012c82

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,14 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 # CnE
 BOARD_USES_QCNE := true
 
+# Dexpreopt
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    WITH_DEXPREOPT ?= true
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
+  endif
+endif
+
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2022-09-01
 
