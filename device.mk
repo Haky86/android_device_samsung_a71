81f2060ddeaca67159e9177cf3cf76ee14588a2d
Pooja Kumari <kumarip@codeaurora.org>
2023-09-22 16:06:54 +0200

    a71: Build the IPACM libs from source
    * Needed for IP Catalogue and IP Management.
    
    Change-Id: I36a36971ca67ef039c52cc88756e3ca1fe3f2e1c

--- device.mk
+++ device.mk
@@ -315,6 +315,13 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor \
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal \
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/usr/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
