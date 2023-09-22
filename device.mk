33ff4e359af202185df985aefd1bbbf567453773
Jan Altensen <info@stricted.net>
2023-09-22 16:24:44 +0200

    a71: Build the Samsung touch service from source
    * Built from hardware/samsung.
    
    Change-Id: Id8b8d8ea5c081181a1643434a5698c07be2f7587

--- device.mk
+++ device.mk
@@ -634,6 +634,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti \
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.samsung \
+
 # WiFi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
