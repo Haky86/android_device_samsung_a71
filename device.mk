84c8b651a54248197fed71a1e4c310d48a60381b
shoudil <stephenli@codeaurora.org>
2023-09-22 16:15:35 +0200

    a71: Build the QTI vendor framework detect packages
    Change-Id: I9ceef01ef08f30730704adcdb392b528613817a8

--- device.mk
+++ device.mk
@@ -491,6 +491,10 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     libjson \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
+    libvndfwk_detect_jni.qti.vendor \
 
 # QTI
 PRODUCT_COPY_FILES += \
