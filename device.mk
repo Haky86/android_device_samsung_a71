954d448ea93fcf0e4a57dedb0df61033b599814f
LuK1337 <priv.luk@gmail.com>
2023-09-22 17:00:49 +0200

    a71: RIP com.dsi.ant.antradio_library
    * No longer shipped by default with lineage-19.0.
    
    Change-Id: I0d01d76974acb0a836962c73aca9c04598b57f8b

--- device.mk
+++ device.mk
@@ -42,8 +42,6 @@ PRODUCT_PACKAGES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
-    com.dsi.ant.antradio_library \
-    com.dsi.ant@1.0.vendor \
     com.qualcomm.qti.ant@1.0 \
 
 # Atrace
