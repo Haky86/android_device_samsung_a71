80bc2c2689100f44400eebadfc35036e72494c4c
LuK1337 <priv.luk@gmail.com>
2023-09-22 17:01:04 +0200

    a71: Disable APEX compression
    * Kernel panics on msm-4.14 + ext4.
    
    Change-Id: Iadf8ad14aee8f94d0f3f0673c7a2b4598ccf05a7

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.qualcomm.qti.ant@1.0 \
 
+# APEX
+PRODUCT_COMPRESSED_APEX := false
+
 # Atrace
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service \
