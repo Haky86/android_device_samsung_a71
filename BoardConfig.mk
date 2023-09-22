93b5ed487a25448493487bb9731834c7298305b7
Haky86 <hakamelassouad@gmail.com>
2023-09-22 15:06:22 +0200

    a71: Setup default kernel-inline flags
    * The cmdline parameters has been taken from a71naxx-user 12 SP1A.210812.016 A715FXXU8CVI3 release-keys boot image.
    
    Change-Id: Ie3b4545c913d935e95af032918433bf444890eea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,6 +136,24 @@ DEVICE_MATRIX_FILE := \
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_a71
 TARGET_RECOVERY_DEVICE_MODULES := libinit_a71
 
+# Kernel
+BOARD_BOOT_HEADER_VERSION := 2
+BOARD_DTB_OFFSET := 0x01F00000
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_MKBOOTIMG_ARGS := --dtb_offset $(BOARD_DTB_OFFSET) --header_version $(BOARD_BOOT_HEADER_VERSION) --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := a71_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/sm7150
+
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2022-09-01
 
