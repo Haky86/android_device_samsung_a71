2f787f10d7a8cf888eb7b150578aafdb5a73af7a
Haky86 <hakamelassouad@gmail.com>
2023-09-22 15:04:46 +0200

    a71: Setup HIDL manifest and matrix configs
    * Taken from a71naxx-user 12 SP1A.210812.016 A715FXXU8CVI3 release-keys.
    
    Change-Id: I9b10a33a49441ab4582505b1ab5ccc3e788c56c2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,13 @@ TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
 VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
 
+# HIDL
+DEVICE_MANIFEST_FILE := \
+    $(DEVICE_PATH)/manifest.xml
+
+DEVICE_MATRIX_FILE := \
+    $(DEVICE_PATH)/compatibility_matrix.xml
+
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2022-09-01
 
