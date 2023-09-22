1ae4cfaef506d928e948739b158b0679daf5b7be
Haky86 <hakamelassouad@gmail.com>
2023-09-22 16:37:56 +0200

    a71: Drop AOSP camera provider service
    * It's not working for us anyways, simply never did.
    
    Change-Id: Iab04be5a2b6ea1e77d37f1ac4db12bc9644c768e

--- device.mk
+++ device.mk
@@ -157,7 +157,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.7 \
     android.hardware.camera.device@3.7.vendor \
     android.hardware.camera.provider@2.5-impl \
-    android.hardware.camera.provider@2.5-service_64 \
     android.hardware.camera.provider@2.7 \
     android.hardware.camera.provider@2.7.vendor \
     libcamera2ndk_vendor \
