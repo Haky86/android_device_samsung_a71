1a8944cb36dc9e1a2ef049e7ca463c7baea41b34
Adam Shih <adamshih@google.com>
2023-09-23 21:34:55 +0200

    a71: Create a makefile that holds non AB device setting
    Bug: 189894781
    Test: build ROM with the binary inside
    Change-Id: If6e02ad073c14f9840a987757ce311f0f6223d6e

--- device.mk
+++ device.mk
@@ -19,6 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
